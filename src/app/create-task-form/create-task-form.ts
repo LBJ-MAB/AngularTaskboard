0863ce36eccc1476d240632045a7fea446de4822
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-12-03 16:11:56 +0000

    using a reactive form instead

--- src/app/create-task-form/create-task-form.ts
+++ src/app/create-task-form/create-task-form.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Guid } from 'guid-typescript';
 import { ITask } from '../Interfaces/ITask';
-import { FormsModule } from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { TaskService } from '../services/task-service';
 
 
@@ -10,50 +10,35 @@ import { TaskService } from '../services/task-service';
   selector: 'app-create-task-form',
   imports: [
     RouterLink,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './create-task-form.html',
   styleUrl: './create-task-form.css',
 })
-export class CreateTaskForm {
+export class CreateTaskForm implements OnInit {
 
-  taskTitle: string = '';
-  dueDate: string = '';
-  description? : string;
-  priority? : number | null = null;
+  formGroup: FormGroup = new FormGroup({});
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      title: ['', Validators.required],
+      dueDate: ['', Validators.required],
+      description: [''],
+      priority: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.formGroup.valid) {
+      console.log(this.formGroup);
+      this.taskService.addTask(this.formGroup.value);
+    }
+  }
 
   handleCancelClick() : void {
-    this.clearInputs();
-  }
-
-  handlePrioritySelected(optionValue: string) {
-    let value = parseInt(optionValue);
-    this.priority = value === 0 ? null : value;
-  }
-
-  handleSubmitClick() : void {
-
-    console.log(this.priority);
-
-    let task: ITask = {
-      title: this.taskTitle,
-      dueDate: this.dueDate,
-      id: Guid.create().toString(),
-      isComplete: false,
-      description: this.description,
-      priority: this.priority
-    }
-
-    this.taskService.addTask(task);
-    this.clearInputs();
-  }
-
-  clearInputs() {
-    this.taskTitle = '';
-    this.dueDate = '';
-    this.description = '';
-    this.priority = null;
+    this.formGroup.reset();
   }
 }
